421222710c45b539362bb38246afef1e931b3bad
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2026-04-19 16:56:25 -0500

    Update yespower.c

--- algo/yespower.c
+++ algo/yespower.c
@@ -45,7 +45,7 @@
 void yespower_hash( const char *input, char *output, uint32_t len )
 {
     static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-    yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
     static yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 2048,
@@ -53,7 +53,7 @@ void yespower_hash( const char *input, char *output, uint32_t len )
         .pers = NULL,
         .perslen = 0
     };
-    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
 int scanhash_yespower( int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done )
@@ -93,7 +93,7 @@ int scanhash_yespower( int thr_id, struct work *work, uint32_t max_nonce, uint64
 void yespowerIC_hash( const char *input, char *output, uint32_t len )
 {
     static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-    yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
     static yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 2048,
@@ -101,7 +101,7 @@ void yespowerIC_hash( const char *input, char *output, uint32_t len )
         .pers = (const uint8_t *)"IsotopeC",
         .perslen = 8
     };
-    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
 int scanhash_yespowerIC( int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done )
@@ -141,7 +141,7 @@ int scanhash_yespowerIC( int thr_id, struct work *work, uint32_t max_nonce, uint
 void yespowerIOTS_hash( const char *input, char *output, uint32_t len )
 {
     static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-    yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
     static yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 2048,
@@ -149,7 +149,7 @@ void yespowerIOTS_hash( const char *input, char *output, uint32_t len )
         .pers = (const uint8_t *)"Iots is committed to the development of IOT",
         .perslen = 43
     };
-    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
 int scanhash_yespowerIOTS( int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done )
@@ -189,7 +189,7 @@ int scanhash_yespowerIOTS( int thr_id, struct work *work, uint32_t max_nonce, ui
 void yespowerITC_hash( const char *input, char *output, uint32_t len )
 {
     static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-    yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
     static yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 2048,
@@ -197,7 +197,7 @@ void yespowerITC_hash( const char *input, char *output, uint32_t len )
         .pers = (const uint8_t *)"InterITC",
         .perslen = 8
     };
-    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
 int scanhash_yespowerITC( int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done )
@@ -237,7 +237,7 @@ int scanhash_yespowerITC( int thr_id, struct work *work, uint32_t max_nonce, uin
 void yespowerLITB_hash( const char *input, char *output, uint32_t len )
 {
     static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-    yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
     static yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 2048,
@@ -245,7 +245,7 @@ void yespowerLITB_hash( const char *input, char *output, uint32_t len )
         .pers = (const uint8_t *)"LITBpower: The number of LITB working or available for proof-of-work mining",
         .perslen = 73
     };
-    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
 int scanhash_yespowerLITB( int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done )
@@ -285,7 +285,7 @@ int scanhash_yespowerLITB( int thr_id, struct work *work, uint32_t max_nonce, ui
 void yespowerLNC_hash( const char *input, char *output, uint32_t len )
 {
     static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-    yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
     static yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 2048,
@@ -293,7 +293,7 @@ void yespowerLNC_hash( const char *input, char *output, uint32_t len )
         .pers = (const uint8_t *)"LTNCGYES",
         .perslen = 8
     };
-    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
 int scanhash_yespowerLNC( int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done )
@@ -341,13 +341,13 @@ enum YespowerParamsType paramsType = YESPOWER_PARAMS_MAGPIECOIN;
 
 
         static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-        yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+        yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
 
     static yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 2048,
         .r = 32,
-        .pers = "Magpies are birds of the Corvidae family.",
+        .pers = (const uint8_t *)"Magpies are birds of the Corvidae family.",
         .perslen = 41
     };
 
@@ -357,7 +357,7 @@ enum YespowerParamsType paramsType = YESPOWER_PARAMS_MAGPIECOIN;
          default:
             break;
     }
-    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
 int scanhash_yespowerMGPC( int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done )
@@ -397,7 +397,7 @@ int scanhash_yespowerMGPC( int thr_id, struct work *work, uint32_t max_nonce, ui
 void yespowerR16_hash( const char *input, char *output, uint32_t len )
 {
     static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-    yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
     static yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 4096,
@@ -405,7 +405,7 @@ void yespowerR16_hash( const char *input, char *output, uint32_t len )
         .pers = NULL,
         .perslen = 0
     };
-    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
 int scanhash_yespowerR16( int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done )
@@ -445,7 +445,7 @@ int scanhash_yespowerR16( int thr_id, struct work *work, uint32_t max_nonce, uin
 void yespowerSUGAR_hash( const char *input, char *output, uint32_t len )
 {
     static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-    yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
     static yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 2048,
@@ -453,7 +453,7 @@ void yespowerSUGAR_hash( const char *input, char *output, uint32_t len )
         .pers = (const uint8_t *)"Satoshi Nakamoto 31/Oct/2008 Proof-of-work is essentially one-CPU-one-vote",
         .perslen = 74
     };
-    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
 int scanhash_yespowerSUGAR( int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done )
@@ -493,7 +493,7 @@ void yespowerTIDE_hash( const char *input, char *output, uint32_t len)
  {
      {
 	static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-        yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+        yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
      }
  }
 
@@ -533,7 +533,7 @@ int scanhash_yespowerTIDE(int thr_id, struct work *work, uint32_t max_nonce, uin
 void yespowerURX_hash( const char *input, char *output, uint32_t len )
 {
     static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-    yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
     static yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 2048,
@@ -541,7 +541,7 @@ void yespowerURX_hash( const char *input, char *output, uint32_t len )
         .pers = (const uint8_t *)"UraniumX",
         .perslen = 8
     };
-    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
 int scanhash_yespowerURX( int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done )
@@ -581,7 +581,7 @@ int scanhash_yespowerURX( int thr_id, struct work *work, uint32_t max_nonce, uin
 void cpupower_hash( const char *input, char *output, uint32_t len )
 {
     static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
-    yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &v1, (yespower_binary_t*)output );
     static yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 2048,
@@ -589,7 +589,7 @@ void cpupower_hash( const char *input, char *output, uint32_t len )
         .pers = (const uint8_t *)"CPUpower: The number of CPU working or available for proof-of-work mining",
         .perslen = 73
     };
-    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+    yespower_tls( (const uint8_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
 int scanhash_cpupower(int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done)
